2ce7f45f69d63ac218a7febebb1a5c975f960fd8
Haris Ahmad <harisahmad.188@gmail.com>
2020-03-29 03:41:11 -0400

    second

--- src/tweeter.ts
+++ src/tweeter.ts
@@ -8,7 +8,7 @@ const writeFile = util.promisify(fs.writeFile);
 
 require('dotenv').config();
 
-const connectionString = process.env.DB_URI;
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
     connectionString: connectionString,
     max:20,
